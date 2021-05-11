b5e6067c2f3a9f68622aebaa530628ac72331db4
Andy Chang <ychang118@my.bcit.ca>
2021-05-11 16:31:46 -0700

    Modified firebase read function to update on each doc change. Added styling to group-msgs page.

--- js/firebase-queries.js
+++ js/firebase-queries.js
@@ -18,13 +18,27 @@ export function createUser() {
     });
 }
 
-export function displayMsgs() {
-    groupRef.doc("group1").collection("groupMessages").get()
-    .then(querySnapshot => {
-        querySnapshot.forEach(doc => {
-            let msg = `<h3>${doc.data().sentBy}</h3><p>${doc.data().message}</p><aside>${doc.data().sentAt}</aside>`;
-            $("#messages").append(msg);
-            console.log(doc.id, " => ", doc.data());
+export function displayMsgs(groupID) {
+    firebase.auth().onAuthStateChanged(function(user) {
+        groupRef.doc("group1").collection("groupMessages")
+        .orderBy("sentAt")
+        //Tech-Tip 016 from Comp 1800
+        //Author: Carly Orr
+        //Source: https://www.notion.so/Tech-Tip-016-How-do-I-listen-to-new-documents-added-to-a-collection-16469db1a9d7451f8d0c2012bfd084ee
+        .onSnapshot((snapshot) => {
+            snapshot.docChanges().forEach(function (change) {
+                if (change.type === "added") {
+                    let msg = "";
+                    snapshot.forEach(function (doc) {
+                        if (doc.data().uid === user.uid) {
+                            msg += `<div class="sent"><p class="name">${doc.data().sentBy}</p><p class="message">${doc.data().message}</p><aside class="time">${doc.data().sentAt}</aside></div>`;
+                        } else {
+                            msg += `<div class="incoming"><p class="name">${doc.data().sentBy}</p><p class="message">${doc.data().message}</p><aside class="time">${doc.data().sentAt}</aside></div>`;
+                        }
+                    });
+                    $("#messages").html(msg);
+                }
+            });
         });
     });
 }
@@ -34,10 +48,16 @@ export function sendMsg(msgToSend) {
         usersRef.doc(user.uid).get()
         .then(function (doc) {
             const userName = doc.data().name;
+            //Intl.DateTimeFormat() constructor from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat
+            const date = new Intl.DateTimeFormat("en", {
+                dateStyle: "short",
+                timeStyle: "medium"
+            }).format(Date.now());
             groupsRef.doc("group1").collection("groupMessages").add({
                 sentBy: userName,
-                sentAt: Date.now(),
-                message: msgToSend.value
+                sentAt: date,
+                message: msgToSend.value,
+                uid: user.uid
             });
             // .then(() => {
             //     window.location.href = "schedule.html";
