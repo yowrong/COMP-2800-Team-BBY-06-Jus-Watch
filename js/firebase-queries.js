68f30429c8346ec31203111ef50569fd3355dcd5
Andy Chang <ychang118@my.bcit.ca>
2021-05-12 15:04:00 -0700

    Moved renderGroups function from firebase-queries to group-main.

--- js/firebase-queries.js
+++ js/firebase-queries.js
@@ -1,3 +1,5 @@
+import { renderGroups } from "./js/group-main.js";
+
 const db = firebase.firestore();
 const usersRef = db.collection("users");
 const groupRef = db.collection("groups");
@@ -112,28 +114,3 @@ export function displayGroups(groupSection) {
     })
 }
 
-// renders a "Group" card for each group the user is in
-function renderGroups(id, name, desc, groupSection) {
-    let groupCard = "";
-    for (let i = 0; i < id.length; i++) {
-
-        // Bootstrap card template, "Enter Group" button redirects to Group Center page
-        groupCard += `<div class="card mb-3" style="max-width: 540px;">
-                        <div class="row g-0">
-                            <div class="col-md-4">
-                                <!-- <img src="..." alt="..."> -->
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">${name[i]}</h5>
-                                    <p class="card-text">${desc[i]}</p>
-                                    <a href="./index.html?${id[i]}">
-                                    <button id="${id[i]}" type="button" class="btn btn-primary btn-lg enter">Enter Group</button>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>`;
-    groupSection.innerHTML = groupCard;
-    }
-}
