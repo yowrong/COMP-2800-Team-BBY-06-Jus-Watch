82e4541101af75ba0e9a33454177b6891bc8bc43
ZiChun Xu <wudao.lilian@gmail.com>
2021-05-13 16:30:24 +0800

    Update group-centre.js

--- group-centre.js
+++ group-centre.js
@@ -26,7 +26,6 @@ function wait(ms) {
 }
 
 var groupNo = [];
-
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
 
@@ -41,8 +40,6 @@ firebase.auth().onAuthStateChanged(function (user) {
         </div>`);
                     console.log(groupNo);
                     console.log(i);
-
-                    
                 }
             } else {
                 console.log("No such document!");
@@ -60,7 +57,6 @@ firebase.auth().onAuthStateChanged(function (user) {
                     });
                 });
             }
-            
         }).catch((error) => {
             console.log("Error getting document:", error);
         });
@@ -68,164 +64,164 @@ firebase.auth().onAuthStateChanged(function (user) {
     }
 });
 
-// // gets group Info from URL and queries Firestore
-// function getGroup(groupID, inviteMsg) {
-//     groupRef.doc(groupID).get()
-//     .then(function(doc) {
-//       let id = doc.data().groupId;
-//       let name = doc.data().groupName;
-//       let desc = doc.data().groupDescription;
+// gets group Info from URL and queries Firestore
+function getGroup(groupID, inviteMsg) {
+    groupRef.doc(groupID).get()
+    .then(function(doc) {
+      let id = doc.data().groupId;
+      let name = doc.data().groupName;
+      let desc = doc.data().groupDescription;
 
-//       displayGroup(id, name, desc);
-//       shareLink(id);
-//       displayMovies(id, movieSection);
+      displayGroup(id, name, desc);
+      shareLink(id);
+      displayMovies(id, movieSection);
 
-//     })
-//   }
+    })
+  }
 
-// getGroup(groupID, inviteMsg);
+getGroup(groupID, inviteMsg);
 
-// // queries groupMembers subcollection to get number of members in group
-// function getNumOfMembers() {
-//     groupRef.doc(groupID).collection("groupMembers").get()
-//     .then(function(doc) {
-//         let numOfMembers = doc.size;
-//         console.log("num of members: " + numOfMembers);
-//         checkVotes(numOfMembers);
-//     });
+// queries groupMembers subcollection to get number of members in group
+function getNumOfMembers() {
+    groupRef.doc(groupID).collection("groupMembers").get()
+    .then(function(doc) {
+        let numOfMembers = doc.size;
+        console.log("num of members: " + numOfMembers);
+        checkVotes(numOfMembers);
+    });
 
-// }
-// getNumOfMembers();
+}
+getNumOfMembers();
 
-// // checks to see if everyone in group has voted, if yes, shows "Movie of the Week"
-// function checkVotes(members) {
-//     groupRef.doc(groupID).get()
-//     .then(function(doc) {
-//         if (doc.data().totalVotes == members) {
-//             console.log("equal votes");
-//             console.log(doc.data().totalVotes);
-//             console.log("IF num of members: " + members);
+// checks to see if everyone in group has voted, if yes, shows "Movie of the Week"
+function checkVotes(members) {
+    groupRef.doc(groupID).get()
+    .then(function(doc) {
+        if (doc.data().totalVotes == members) {
+            console.log("equal votes");
+            console.log(doc.data().totalVotes);
+            console.log("IF num of members: " + members);
 
-//             getWinningMovie(groupID);
+            getWinningMovie(groupID);
 
-//         } 
-//     })
-// }
+        } 
+    })
+}
 
-// // gets movie with most votes in nominatedMovies collection
-// function getWinningMovie(id) {
-//     groupRef.doc(id).collection("nominatedMovies")
-//     .orderBy("numOfVotes", "desc").limit(1)
-//     .get()
-//     .then(function(snap) {
-//         snap.forEach(function(movie) {
-//             let title = movie.data().movieTitle;
-//             let desc = movie.data().movieDescription;
-//             let year = movie.data().movieYear;
-//             let pic = movie.data().moviePoster;
+// gets movie with most votes in nominatedMovies collection
+function getWinningMovie(id) {
+    groupRef.doc(id).collection("nominatedMovies")
+    .orderBy("numOfVotes", "desc").limit(1)
+    .get()
+    .then(function(snap) {
+        snap.forEach(function(movie) {
+            let title = movie.data().movieTitle;
+            let desc = movie.data().movieDescription;
+            let year = movie.data().movieYear;
+            let pic = movie.data().moviePoster;
 
-//             renderWinningMovie(title, desc, year, id, pic, movieSection)
-//         })
-//     })
-// }
+            renderWinningMovie(title, desc, year, id, pic, movieSection)
+        })
+    })
+}
 
-// // changes "nominated movies" section to "movie of the week", generates the winning movie
-// function renderWinningMovie(title, desc, year, id, pic, movies) {
-//     let movieCard = "";
+// changes "nominated movies" section to "movie of the week", generates the winning movie
+function renderWinningMovie(title, desc, year, id, pic, movies) {
+    let movieCard = "";
 
-//         movieCard += `<div class="card winningMovie">
-//         <img src="${pic}" class="card-img-top" alt="${title}">
-//         <div class="card-body">
-//           <h5 class="card-title">${title}</h5>
-//           <p class="card-text">${desc}</p>
-//         </div>
-//         <div class="card-footer">
-//       <small class="text-muted">${year}</small>
-//     </div>
-//       </div>`;
+        movieCard += `<div class="card winningMovie">
+        <img src="${pic}" class="card-img-top" alt="${title}">
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class="card-text">${desc}</p>
+        </div>
+        <div class="card-footer">
+      <small class="text-muted">${year}</small>
+    </div>
+      </div>`;
 
-//     movies.innerHTML = movieCard;
-//     movieCenterTitle.innerText = "Movie of the Week"
-// }
+    movies.innerHTML = movieCard;
+    movieCenterTitle.innerText = "Movie of the Week"
+}
 
 
-// // displays Group name and description
-// function displayGroup(id, name, desc) {
-//     groupName.innerText = name;
-//     groupDesc.innerText = desc;
-// }
+// displays Group name and description
+function displayGroup(id, name, desc) {
+    groupName.innerText = name;
+    groupDesc.innerText = desc;
+}
 
-// // generates correct links for the buttons
-// function shareLink(groupID) {
-//     share.setAttribute("value", `https://www.JusWatch.com/group_main.html?${groupID}`);
-//     nominateBtn.addEventListener("click", function(e) {
-//         e.preventDefault;
-//         window.location.href = `nominate.html?${groupID}`;
-//     })
-//     voteBtn.addEventListener("click", function(e) {
-//         e.preventDefault;
-//         window.location.href = `vote.html?${groupID}`;
-//     })
-//     chatBtn.addEventListener("click", function(e) {
-//         e.preventDefault;
-//         window.location.href = `group-msgs.html?${groupID}`;
-//     })
-// }
+// generates correct links for the buttons
+function shareLink(groupID) {
+    share.setAttribute("value", `https://www.JusWatch.com/group_main.html?${groupID}`);
+    nominateBtn.addEventListener("click", function(e) {
+        e.preventDefault;
+        window.location.href = `nominate.html?${groupID}`;
+    })
+    voteBtn.addEventListener("click", function(e) {
+        e.preventDefault;
+        window.location.href = `vote.html?${groupID}`;
+    })
+    chatBtn.addEventListener("click", function(e) {
+        e.preventDefault;
+        window.location.href = `group-msgs.html?${groupID}`;
+    })
+}
 
-// // displays nominated movies from group's collection
-// function displayMovies(id, movies) {
-//     groupRef.doc(id).collection("nominatedMovies").get()
-//     .then((doc) => { 
+// displays nominated movies from group's collection
+function displayMovies(id, movies) {
+    groupRef.doc(id).collection("nominatedMovies").get()
+    .then((doc) => { 
 
-//         // create arrays of all movies and movie items in group collection
-//         let movieId = [];
-//         let movieName = [];
-//         let movieDesc = [];
-//         let movieYear = [];
-//         let moviePic = [];
+        // create arrays of all movies and movie items in group collection
+        let movieId = [];
+        let movieName = [];
+        let movieDesc = [];
+        let movieYear = [];
+        let moviePic = [];
 
-//         // if no nominated movies
-//         if (doc.size == 0) {
-//             movies.innerHTML = "No Movies Nominated Yet!"
+        // if no nominated movies
+        if (doc.size == 0) {
+            movies.innerHTML = "No Movies Nominated Yet!"
 
-//         } else {
-//             doc.forEach((movie) => {
-//                 // console.log("movie: " + movie.data().movieTitle);
+        } else {
+            doc.forEach((movie) => {
+                // console.log("movie: " + movie.data().movieTitle);
 
-//                 // each movie gets added to the arrays
-//                 movieId.push(movie.data().imdbID)
-//                 movieName.push(movie.data().movieTitle)
-//                 movieDesc.push(movie.data().movieDescription)
-//                 movieYear.push(movie.data().movieYear)
-//                 moviePic.push(movie.data().moviePoster)
+                // each movie gets added to the arrays
+                movieId.push(movie.data().imdbID)
+                movieName.push(movie.data().movieTitle)
+                movieDesc.push(movie.data().movieDescription)
+                movieYear.push(movie.data().movieYear)
+                moviePic.push(movie.data().moviePoster)
 
-//             })
-//             renderMovies(movieName, movieDesc, movieYear, movieId, moviePic, movies);
-//         }
+            })
+            renderMovies(movieName, movieDesc, movieYear, movieId, moviePic, movies);
+        }
 
-//     })
-//     .catch((err) => {
-//         throw err;
-//     })
-// }
+    })
+    .catch((err) => {
+        throw err;
+    })
+}
 
-// // creates card for each movie in collection
-// function renderMovies(title, desc, year, id, pic, movies) {
-//     let movieCard = `<div class="card-group">`;
+// creates card for each movie in collection
+function renderMovies(title, desc, year, id, pic, movies) {
+    let movieCard = `<div class="card-group">`;
 
-//     for (let i = 0; i < id.length; i++) {
-//         movieCard += `<div class="card">
-//         <img src="${pic[i]}" class="card-img-top" alt="${title[i]}">
-//         <div class="card-body">
-//           <h5 class="card-title">${title[i]}</h5>
-//           <p class="card-text">${desc[i]}</p>
-//         </div>
-//         <div class="card-footer">
-//       <small class="text-muted">${year[i]}</small>
-//     </div>
-//       </div>`;
-//     }
-//     movieCard += "</div>";
-//     movies.innerHTML = movieCard;
-// }
+    for (let i = 0; i < id.length; i++) {
+        movieCard += `<div class="card">
+        <img src="${pic[i]}" class="card-img-top" alt="${title[i]}">
+        <div class="card-body">
+          <h5 class="card-title">${title[i]}</h5>
+          <p class="card-text">${desc[i]}</p>
+        </div>
+        <div class="card-footer">
+      <small class="text-muted">${year[i]}</small>
+    </div>
+      </div>`;
+    }
+    movieCard += "</div>";
+    movies.innerHTML = movieCard;
+}
 
