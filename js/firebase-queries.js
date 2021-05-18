ed82c67d00d2567c93cd146a6ae681ef0bae30ec
Josef Murillo <jdmurillo93@gmail.com>
2021-05-18 13:50:15 -0700

    trying to add validation onto create-group form, removed img div from group card on group-main.html

--- js/firebase-queries.js
+++ js/firebase-queries.js
@@ -381,15 +381,12 @@ export function renderGroups(id, name, desc, groupSection) {
         // Bootstrap card template, "Enter Group" button redirects to Group Center page
         groupCard += `<div class="card mb-3" style="max-width: 540px;">
                         <div class="row g-0">
-                            <div class="col-md-4">
-                                <!-- <img src="..." alt="..."> -->
-                            </div>
                             <div class="col-md-8">
                                 <div class="card-body">
                                     <h5 class="card-title">${name[i]}</h5>
                                     <p class="card-text">${desc[i]}</p>
                                     <a href="./group-centre.html?${id[i]}">
-                                        <button id="${id[i]}" type="button" class="btn btn-primary btn-lg enter">Enter Group</button>
+                                        <button id="${id[i]}" type="button" class="btn btn-danger btn-lg enter">Enter Group</button>
                                     </a>
                                 </div>
                             </div>
