3d4d123c06f7da2c5a9ef2735becc3cae49b0055
ZIchunXu <wudao.lilian@gmail.com>
2021-05-22 01:33:25 +0800

    add css for movieresult page

--- js/searchtest.js
+++ js/searchtest.js
@@ -70,34 +70,46 @@ function getMovie() {
 
       let movie = response.data;
       let output = `
-          <div class="moviedscrpt">
-            <div class="col-md-4">
-              <img src="${movie.Poster}" class="thumbnail">
-            </div>
-            <div class="col-md-8">
-              <h2  style = "color: white">${movie.Title}</h2>
-              <ul class="list-group">
-                <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
-                <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
-                <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
-                <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
-                <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
-                <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
-                <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
-              </ul>
-            </div>
-          
-          <div class="">
-            <div class="well">
-              <h3 style = "color: white" >Plot</h3>
-              <p style = "color: white">${movie.Plot}</p>
-              <hr>
-              <a href="post-review.html" target="_blank" class="btn btn-danger">Leave a Comment</a>
-              <a href="" onclick="addFavourite(event)"; id = "Addfavourite" class="btn btn-danger">Add Favourite</a>
-              <a href="profile_favorite.html" ; class="btn btn-danger">Favourite List</a>
+      <div class="moviedscrpt">
+        <div class="centerbox">
+          <img src="${movie.Poster}" class="thumbnail">
+          <h2  style = "color: white;">${movie.Title}</h2>
+        </div>
+            <ul class="list-group">
+              <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+              <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+              <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+              <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+              <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+              <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+              <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+            </ul>
+        <div class="well" style="margin:4%">
+            <h3 style = "color: white" >Plot</h3>
+            <p style = "color: white">${movie.Plot}</p>
+            <hr>
+            <div class="centerbox">
+            <a href="post-review.html" target="_blank" class="anotest">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              Leave a Comment</a>
+            <a href="" onclick="addFavourite(event)"; id = "Addfavourite" class="anotest">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              Add Favourite</a>
+            <a href="profile_favorite.html" ; class="anotest">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              Favourite List</a>
               </div>
-          </div>
-          </div>
+        </div>
+      </div>
         `;
       console.log(1 + output);
       $('#movie').html(output);
@@ -149,8 +161,7 @@ function addFavourite(e) {
             });
             $('#Addfavourite').text('Remove Favourite');
             addFavBtn = $("#Addfavourite").text();
-          }
-          else if (addFavBtn.localeCompare("Remove Favourite") == 0) {
+          } else if (addFavBtn.localeCompare("Remove Favourite") == 0) {
             db.collection("users").doc(user.uid).update({
               "favouriteLists": firebase.firestore.FieldValue.arrayRemove(movieId),
             });
@@ -163,6 +174,3 @@ function addFavourite(e) {
 
   //window.location = "\profile_favorite.html"
 }
-
-
-
