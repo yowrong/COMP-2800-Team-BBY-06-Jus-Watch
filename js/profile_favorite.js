5a507676dedf872472499c147cf8edf7ee5770b0
Andy Chang <ychang118@my.bcit.ca>
2021-05-25 16:04:57 -0700

    Move movieSelected function to firebase-queries.

--- js/profile_favorite.js
+++ js/profile_favorite.js
@@ -9,10 +9,3 @@ const backBtn = document.getElementById('backBtn');
 backBtn.addEventListener('click', function(e) {
     window.history.back();
 })
-
-//redirect to the detail page
-function movieSelected(id) {
-    sessionStorage.setItem('movieId', id);
-    window.location = "movieresult.html?" + id;
-    return false;
-}
